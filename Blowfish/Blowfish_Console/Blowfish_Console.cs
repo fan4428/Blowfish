fe1a74162e772944b19d539071033b89dc64f134
uneforce <uneforce@yandex.ru>
2016-12-22 23:30:15 +0300

    Add files via upload

---
+++ Blowfish/Blowfish_Console/Blowfish_Console.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlowfishLib;
+
+namespace Blowfish_Console
+{
+    class Blowfish_Console
+    {
+        static void Main(string[] args)
+        {
+            int exit = 1;
+            string value;
+            string key = "{123ABC9F-AFBC-36DC-8FF8-00BDAFF1584DGFQQ}";
+            string encrypted;
+            string decipher;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Input value:    ");
+                value = Convert.ToString(Console.ReadLine());
+                Blowfish blowfish = new Blowfish(key);
+                encrypted = blowfish.Encipher(value);
+                Console.WriteLine("Encrypted value:     " + encrypted);
+                decipher = blowfish.Decipher(encrypted);
+                Console.WriteLine("Decipher value:     " + decipher);
+                Console.WriteLine("Input 0 to exit:    ");
+                exit = Convert.ToInt32(Console.ReadLine());
+            }
+            while (exit != 0);
+        }
+    }
+}
---
+++ Blowfish_Test/Blowfish_test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlowfishLib;
+
+namespace Blowfish_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string key = "{123ABC9F-AFBC-36DC-8FF8-00BDAFF1584DGFQQ}";
+        private string value = "BlowFish";
+        private string encrypted = "NuLZ/cP9jQoCPmJ2vtTspA==";
+
+        [TestMethod]
+        public void Encipher()
+        {
+            Blowfish blowfish = new Blowfish(key);
+            Assert.AreEqual(blowfish.Encipher(value), encrypted);
+        }
+        [TestMethod]
+        public void Decipher()
+        {
+            Blowfish blowfish = new Blowfish(key);
+            Assert.AreEqual(blowfish.Decipher(encrypted), value);
+        }
+
+    }
+}
